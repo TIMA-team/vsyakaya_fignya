37fa7f7c65537f4e0b4e14baa3bdd1c525277174
Unknown <48orusef@gmail.com>
2018-07-12 22:17:49 +0300

    Токены с частотой

--- ne.py
+++ ne.py
@@ -1,31 +1,30 @@
 import re
-import names
+from collections import defaultdict
+import json
+from vsyakaya_fignya import names
 
 
-tokens = [set(), set(), set()]
+tokens = [defaultdict(int), defaultdict(int), defaultdict(int)]
 counter = 0
-reg = r'\"\d+\"'
 reg2 = r'{\S*}'
-
 
 with open(names.dataset, 'r', encoding='utf-8') as f:
     for line in f:
         counter += 1
         if counter % 10000 == 0:
             print(counter)
-        ll = re.findall(reg2, line)
-        for i in range(3):
-            for el in re.findall(reg, ll[i]):
-                tokens[i].add(el[1:-1])
+        for id, el in enumerate(re.findall(reg2, line)):
+            for tk, tv in json.loads(el).items():
+                tokens[id][tk] += tv
 
-with open(names.tokens1, 'w', encoding='utf-8') as t1:
-    for el in tokens[0]:
-        t1.write(el + '\n')
+with open(names.tokens1f, 'w', encoding='utf-8') as t1:
+    for k, v in tokens[0]:
+        t1.write(str(k) + '\t' + str(v) + '\n')
 
-with open(names.tokens2, 'w', encoding='utf-8') as t2:
-    for el in tokens[1]:
-        t2.write(el + '\n')
+with open(names.tokens2f, 'w', encoding='utf-8') as t2:
+    for k, v in tokens[1]:
+        t2.write(str(k) + '\t' + str(v) + '\n')
 
-with open(names.tokens3, 'w', encoding='utf-8') as t3:
-    for el in tokens[2]:
-        t3.write(el + '\n')
+with open(names.tokens3f, 'w', encoding='utf-8') as t3:
+    for k, v in tokens[2]:
+        t3.write(str(k) + '\t' + str(v) + '\n')
