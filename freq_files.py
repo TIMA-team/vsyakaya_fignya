7df77f4bdeaa88fc298f6464ad07048f72abb760
Unknown <48orusef@gmail.com>
2018-07-14 19:16:42 +0300

    более быстрое исправление

--- freq_files.py
+++ freq_files.py
@@ -20,20 +20,17 @@ with open(names.tokens3f, 'r', encoding='utf-8') as f:
 fc1 = open(names.fcounter1, 'w', encoding='utf-8')
 fc2 = open(names.fcounter2, 'w', encoding='utf-8')
 fc3 = open(names.fcounter3, 'w', encoding='utf-8')
-fld = open(names.labels_ld, 'w', encoding='utf-8')
+# fld = open(names.labels_ld, 'w', encoding='utf-8')
 # fad = open(names.labels_ld, 'w', encoding='utf-8')
-fom = open(names.othermetrics, 'w', encoding='utf-8')
+# fom = open(names.othermetrics, 'w', encoding='utf-8')
 
 with open(names.v4, 'r', encoding='utf-8') as mf:
     counter = 0
     for line in mf:
         counter += 1
-        if counter % 25000 == 0:
+        if counter % 125000 == 0:
             print(str((counter * 100)//12874345) + '%')
         line_list = line[:-1].split('\t')
-        label = line_list[0]
-        fld.write(line_list[0] + '\n')
-        fom.write(line_list[1] + '\t' + line_list[-1] + '\n')
         for k, el in json.loads(line_list[2]).items():
             fc1.write((t_freq_dicts[0][k] + '\t') * int(el))
         for k, el in json.loads(line_list[3]).items():
@@ -47,6 +44,6 @@ with open(names.v4, 'r', encoding='utf-8') as mf:
 fc1.close()
 fc2.close()
 fc3.close()
-fld.close()
+# fld.close()
 # fad.close()
-fom.close()
+# fom.close()
